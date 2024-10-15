bc2ab52aa5a3157c53bcbeccd8cb5374c69429a0
Raphael Rodrigues <73262792+raphadvlp@users.noreply.github.com>
2024-10-14 23:13:57 -0300

    ajuste firebase

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 // Configurações do Firebase
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyCvBpaefejjMNPTN_A-yV5s6F0_okQFJZk",
+  authDomain: "capsuladotempo-9d755.firebaseapp.com",
+  projectId: "capsuladotempo-9d755",
+  storageBucket: "capsuladotempo-9d755.appspot.com",
+  messagingSenderId: "869092303974",
+  appId: "1:869092303974:web:7d5b69c10147a178ddb9a7",
 };
 
 // Inicializar o Firebase
